653635be7a84238e3a737649341c5cf7ef72f946
PPLATTEN <peter.platten@gov.bc.ca>
2026-05-11 11:19:06 -0700

    Added comments to secrets

--- src/jira_helpers/constants.py
+++ src/jira_helpers/constants.py
@@ -8,11 +8,16 @@ if os.path.exists(envPath):
 
 JIRA_API_URL = os.environ['jira_api_url']
 
+# JIRA Client setup
+# Note: Secret and Token are not both required. Token authentication is in development.
 JIRA_CLIENT_ID = os.environ['jira_client_id']
 JIRA_CLIENT_EMAIL = os.environ['jira_client_email']
 JIRA_CLIENT_SECRET = os.environ['jira_client_secret']
 JIRA_CLIENT_TOKEN = os.environ['jira_client_token']
 
+# JIRA Project specific constants
 JIRA_PROJECT = os.environ['jira_project']
 JIRA_COMPONENT = os.environ['jira_component']
+
+# JIRA Search constants
 JIRA_YOUNGER_THAN_MINUTES = int(os.environ['jira_younger_than_minutes'])
