8c376d824f8fe15d4511d4d68bfe711b0f9e1fb3
PPLATTEN <peter.platten@gov.bc.ca>
2026-05-29 16:43:52 -0700

    Fix: switch env for vars because its repo based not system

--- src/jira_helpers/constants.py
+++ src/jira_helpers/constants.py
@@ -13,17 +13,16 @@ DEBUG_EMAIL = os.getenv('DEBUG_EMAIL', '')
 FROM_EMAIL = os.getenv('FROM_EMAIL', '')
 
 # ---- JIRA ----
-JIRA_API_URL = os.getenv('JIRA_API_URL', '')
+JIRA_API_URL = os.getenv('JIRA_API_URL')
 JIRA_CLIENT_ID = os.getenv('JIRA_CLIENT_ID', '')
 JIRA_CLIENT_EMAIL = os.getenv('JIRA_CLIENT_EMAIL', '')
 JIRA_CLIENT_SECRET = os.getenv('JIRA_CLIENT_SECRET', '')
 JIRA_CLIENT_TOKEN = os.getenv('JIRA_CLIENT_TOKEN', '')
-JIRA_PROJECT = os.getenv('JIRA_PROJECT', '')
-JIRA_COMPONENT = os.getenv('JIRA_COMPONENT', '')
-print(os.getenv('JIRA_YOUNGER_THAN_MINUTES'))
+JIRA_PROJECT = os.getenv('JIRA_PROJECT')
+JIRA_COMPONENT = os.getenv('JIRA_COMPONENT')
 JIRA_YOUNGER_THAN_MINUTES = int(os.getenv('JIRA_YOUNGER_THAN_MINUTES'))
 
 
 # ---- JIRA Test Constants ----
 JIRA_TEST_FILE_PATH = os.getenv('JIRA_TEST_FILE_PATH', '')
-JIRA_TEST_ISSUE_KEY = os.getenv('JIRA_TEST_ISSUE_KEY', '')
+JIRA_TEST_ISSUE_KEY = os.getenv('JIRA_TEST_ISSUE_KEY')
