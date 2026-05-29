09605936309610ed00fe8798b214cc495a7b1cc7
PPLATTEN <peter.platten@gov.bc.ca>
2026-05-29 13:02:38 -0700

    fix: github environment variables not loading

--- src/jira_helpers/constants.py
+++ src/jira_helpers/constants.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv, find_dotenv
 
 envPath = find_dotenv(usecwd=True)
 if envPath:
-    load_dotenv(dotenv_path=envPath)
+    load_dotenv(dotenv_path=envPath, override=False)
 
 # ---- Email / SMTP ----
 SMTP_SERVER = os.getenv('SMTP_SERVER', '')
